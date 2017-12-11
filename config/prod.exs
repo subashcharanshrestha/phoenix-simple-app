40d8c757c6b0914a8830b30ab09b9c64a58637b4
subasvayu <subas@vayu.com.au>
2017-12-11 14:11:43 +0545

    spelling wrong

--- config/prod.exs
+++ config/prod.exs
@@ -72,6 +72,6 @@ config :logger, level: :info
 
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABSE_URL"),
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
